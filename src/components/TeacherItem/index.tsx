73d2ad4d45c7e5efdb0e8fdfa5e17a1b74ae61d2
Samuel Lima <samuellfa3@gmail.com>
2020-08-07 20:55:48 -0300

    Introduce server

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,36 +1,54 @@
 import React from 'react';
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
 
 import './styles.css';
 
-function TeacherItem() {
+export interface Teacher {
+    avatar: string;
+    bio: string;
+    cost: number;
+    id: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    async function createNewConnection() {
+        await api.post('/connections', {
+            user_id: teacher.id
+        });
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars3.githubusercontent.com/u/42184385?s=60&v=4" alt="Samuel" />
+                <img src={teacher.avatar} alt={`Avatar de ${teacher.name}`} />
                 <div>
-                    <strong>Samuel</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
             <p>
-                Teste Teste Teste Teste Teste Teste
-                        <br />
-                        TesteTesteTesteTeste
-                    </p>
+                {teacher.bio}
+            </p>
 
             <footer>
                 <p>
-                    Preço/hora
-                            <strong>R$ 20,00</strong>
+                    Preço/hora <strong>{teacher.cost}</strong>
                 </p>
 
-                <button type="button">
+                <a onClick={createNewConnection} href={` https://wa.me/${teacher.whatsapp}`} target="__blank">
                     <img src={whatsappIcon} alt="Whatsapp" />
-                            Entrar em contato
-                        </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     )
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React  from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -11,6 +13,18 @@ import prupleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
 
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+        async function getTotalConncetions() {
+            const { data } = await api.get('/connections');
+
+            setTotalConnections(data.total);
+        }
+
+        getTotalConncetions();
+    }, []);
+
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -38,7 +52,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas
+                    Total de {totalConnections} conexões já realizadas
                     <img src={prupleHeartIcon}  alt="Coração roxo"/>
                 </span>
             </div>
